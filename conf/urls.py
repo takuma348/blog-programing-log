3ec5bceddccd9327d41f6309b109b1c2e4766f45
python takuma <go.ahead.mabuhay@gmail.com>
2018-07-29 22:54:46 +0900

    debug true

--- conf/urls.py
+++ conf/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 # 開発環境でのメディアファイルの配信設定
