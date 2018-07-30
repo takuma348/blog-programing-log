a761e78679796a24ec5c1a209dc59387555b8908
python takuma <go.ahead.mabuhay@gmail.com>
2018-07-30 23:57:13 +0900

    tweak static

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
-application = Cling(get_wsgi_application()
+application = Cling(get_wsgi_application())
+
 application = DjangoWhiteNoise(application)
